40d396176f211e5335cda3be40bd96437666416e
David Rogers <al.the.x@gmail.com>
2013-03-23 11:11:31 -0400

    @sharievans, @laurencelink

---
+++ 2013-03-23.js
@@ -0,0 +1,37 @@
+var assert=require("assert");
+
+/**
+ * four functions with two parameters:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * 
+ * Must accept positive and negative Numbers (int or 
+ * float) AND zero. Must not make an error!
+ */
+ 
+/**
+ * testing addition():
+ * one | two | out
+ * ====+=====+====
+ *  +1 | +1  | +2
+ *  +1 | +0  | +1
+ *  +1 | -1  | +0
+ *  -1 | +1  | +0
+ *  -1 | +0  | -1
+ *  -1 | -1  | -2
+ *  +0 | +1  | +1
+ *  +0 | +0  | +0
+ *  +0 | -1  | -1
+ */
+ 
+ //Adding addition function
+function addition (a, b) {
+     return 2;
+}
+ 
+ //This is the first attempt at a failing addition test.
+assert (addition (1, 1) == 2, '1 + 1 equals 2');
+ //First red condition
+ 
