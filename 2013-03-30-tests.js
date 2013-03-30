f38bc39be3890d20e27b2fa95a5143985046aa78
David Rogers <al.the.x@gmail.com>
2013-03-30 12:25:31 -0400

    @sharievans @amo-2012

--- 2013-03-30-tests.js
+++ 2013-03-30-tests.js
@@ -6,16 +6,21 @@ var assert = require('assert'),
 /**
  * Tests for division...
  * +1 | +1 | +1
- * +1 | +0 | ??
+ * +1 | +0 | +0
  * +1 | -1 | -1
  * -1 | +1 | -1
- * -1 | +0 | ??
+ * -1 | +0 | +0
  * -1 | -1 | +1
  * +0 | +1 | +0
- * +0 | +0 | ??
+ * +0 | +0 | +0
  * +0 | -1 | +0
  */
 assert(calculator.division(+1, +1) === +1);
-assert(calculator.division(+1, 0) === 0);
+assert(calculator.division(+1, +0) === +0);
 assert(calculator.division(+1, -1) === -1);
-assert(calculator.division(-1, +1) === -1)
+assert(calculator.division(-1, +1) === -1);
+assert(calculator.division(-1, +0) === +0);
+assert(calculator.division(-1, -1) === +1);
+assert(calculator.division(+0, +1) === +0);
+assert(calculator.division(+0, +0) === +0);
+assert(calculator.division(+0, -1) === +0);
