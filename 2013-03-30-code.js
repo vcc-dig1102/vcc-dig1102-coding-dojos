ef3f700539441311cb67097ac7d426a676a672c2
David Rogers <al.the.x@gmail.com>
2013-03-30 11:58:15 -0400

    @uniquely58 @al-the-x

---
+++ 2013-03-30-code.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /*
+    something: function(){
+        return 'something';
+    }
+    */
+    division: function division (a, b){
+     
+        if (b === 0) return 0;
+        
+        if (b === -1) return -1;
+       
+        else  return +1;
+        
+      
+    }
+}
---
+++ 2013-03-30-tests.js
@@ -0,0 +1,21 @@
+// Tests for calculator...
+
+var assert = require('assert'),
+    calculator = require('./2013-03-30-code.js');
+
+/**
+ * Tests for division...
+ * +1 | +1 | +1
+ * +1 | +0 | ??
+ * +1 | -1 | -1
+ * -1 | +1 | -1
+ * -1 | +0 | ??
+ * -1 | -1 | +1
+ * +0 | +1 | +0
+ * +0 | +0 | ??
+ * +0 | -1 | +0
+ */
+assert(calculator.division(+1, +1) === +1);
+assert(calculator.division(+1, 0) === 0);
+assert(calculator.division(+1, -1) === -1);
+assert(calculator.division(-1, +1) === -1)
