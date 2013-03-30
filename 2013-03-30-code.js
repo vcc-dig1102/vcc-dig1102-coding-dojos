a317f3e81cb7060beecdb32c3790f4255dadcb5e
David Rogers <al.the.x@gmail.com>
2013-03-30 12:33:44 -0400

    @amo-2012  @uniquely58

--- 2013-03-30-code.js
+++ 2013-03-30-code.js
@@ -5,13 +5,15 @@ module.exports = {
     }
     */
     division: function division (a, b){
-        if (b === 0) return 0;
-        
-        if (a === 0 && b === +1) return 0;
-        
+       
+                
         if (a === -1 && b === -1) return +1;
 
+        if (a === +0 || b === +0) return +0;
+        
         if (a === -1 || b === -1) return -1;
+        
+        
 
         return +1;
         
