8dba31ef04b2882b47827aea858246af14c1ece4
David Rogers <al.the.x@gmail.com>
2013-03-30 12:46:44 -0400

    Adding Retrospective Notes

--- 2013-03-30-code.js
+++ 2013-03-30-code.js
@@ -5,18 +5,12 @@ module.exports = {
     }
     */
     division: function division (a, b){
-       
-                
         if (a === -1 && b === -1) return +1;
 
         if (a === +0 || b === +0) return +0;
         
         if (a === -1 || b === -1) return -1;
-        
-        
 
         return +1;
-        
-      
     }
 }
--- 2013-03-30-tests.js
+++ 2013-03-30-tests.js
@@ -1,4 +1,28 @@
-// Tests for calculator...
+/**
+ * Retrospective:
+ * 
+ * What did we do well?
+ *   - VERY INCREMENTAL Baby Steps.
+ *   - Split tests and production code.
+ *   - Refactored production code eagerly.
+ *   - Passed all our test cases for division!
+ *   - Solved the "DIV BY ZERO" conundrum!
+ *   - Lined up all the values nicely.
+ * 
+ * What should we improve?
+ *   - Stupidest thing possible.
+ *   - Use larger range of input values?
+ *   - Use a different THEME: Idle Fingers?
+ *   - REMEMBER to commit between pairs!
+ * 
+ * Did we achieve our goals? Why?
+ *   - Learned something about making up math.
+ *   - Learned how to divide files with "require()" and "module.exports"
+ *   - Learned how small Baby Steps really are... O_O
+ *   - Completed our "division()" tests!
+ *   - Logical comparisons: if, and (&&), or (||), else (or not).
+ *   - Learned that order of comparisons matter!
+ */
 
 var assert = require('assert'),
     calculator = require('./2013-03-30-code.js');
