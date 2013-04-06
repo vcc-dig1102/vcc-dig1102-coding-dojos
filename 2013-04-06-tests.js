c9ad5575c614da2c35dd5ca8f5fe63cc019ca39f
David Rogers <al.the.x@gmail.com>
2013-04-06 12:28:41 -0400

    Test plan and implementation plan for 100 Doors

---
+++ 2013-04-06-tests.js
@@ -0,0 +1,15 @@
+var assert = require("assert"),
+    hundredDoors = require("./2013-04-06-code");
+
+/**
+ * test: do we have a hallway?
+ * test: do we have doors in our hallway?
+ * test: do we have 100 doors?
+ * test: are all the doors closed?
+ * do: PASS 1 -- take a walk down the hallway, change the state of the doors
+ * test: are all the doors open?
+ * do: PASS 2 -- take a walk down the hallway, change the state of the doors
+ * test: is every EVEN door closed? is every ODD door open?
+ * do: PASS 3 -- take a walk down the hallway, change the state of the doors
+ * test: does the hallway match our expected hallway state?
+ */
