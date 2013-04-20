6921e8da6f4d6efc36553aedee2b552b08e7ec67
David Rogers <al.the.x@gmail.com>
2013-04-20 10:22:20 -0400

    @amo-2012 @sharievans

--- 2013-04-20-tests.js
+++ 2013-04-20-tests.js
@@ -9,11 +9,17 @@ assert(hundredDoors.hallway.doors);
 
 //do we have 10 doors?
 assert.equal(hundredDoors.hallway.doors.length, 10);
-assert(hundredDoors.hallway.doors.length == 10);
+//  line 11 same as >   'assert(hundredDoors.hallway.doors.length == 10);'
 
 //are all the doors closed?
-
+hundredDoors.hallway.doors.forEach(function(door){
+    assert.equal(door,0);
+    
+        });
+        
 //can we change the state of a door(create toggle function)?
+assert(hundredDoors.hallway.door.toggle
+
 //are all doors in the correct state after first pass?
 //can we tell which pass we are on?
 //are all the doors in the correct state after each pass?
