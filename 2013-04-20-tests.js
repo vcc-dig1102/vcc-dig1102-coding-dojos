c2d7b41cc4024cb4637d619dfe42bd06ec629fd0
David Rogers <al.the.x@gmail.com>
2013-04-20 09:42:53 -0400

    @uniquely58 @rmccaffrey

--- 2013-04-20-tests.js
+++ 2013-04-20-tests.js
@@ -1,3 +1,10 @@
 var assert = require("assert"),
     hundredDoors = require("./2013-04-13-code");
 
+//do we have a hallway?
+//do we have doors in the hallway?
+//do we have 10 doors?
+//are all the doors closed?
+//can we change the state of a door(create toggle function)?
+//are all doors open after first pass?
+//
