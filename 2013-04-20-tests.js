d8b4ae876ab7522149e55b2baf5dd7a537ab6349
David Rogers <al.the.x@gmail.com>
2013-04-20 09:50:29 -0400

    @rmccaffrey @flowerflirt44

--- 2013-04-20-tests.js
+++ 2013-04-20-tests.js
@@ -2,9 +2,12 @@ var assert = require("assert"),
     hundredDoors = require("./2013-04-13-code");
 
 //do we have a hallway?
+assert (hundredDoors.hallway);
+
 //do we have doors in the hallway?
 //do we have 10 doors?
 //are all the doors closed?
 //can we change the state of a door(create toggle function)?
-//are all doors open after first pass?
-//
+//are all doors in the correct state after first pass?
+//can we tell which pass we are on?
+//are all the doors in the correct state after each pass?
