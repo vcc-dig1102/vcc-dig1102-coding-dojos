408eb96e3c64eceeea668bede3afc46b198dfdb6
David Rogers <al.the.x@gmail.com>
2013-04-27 09:42:11 -0400

    Exam time!

---
+++ 2013-04-27-code.js
@@ -0,0 +1,23 @@
+/**
+ * [Roman Numeral Conversion](http://codingdojo.org/cgi-bin/wiki.pl?KataRomanNumerals)
+ *
+ * The Romans were a clever bunch. They conquered most of Europe and ruled it for
+ * hundreds of years. They invented concrete and straight roads and even bikinis. One
+ * thing they never discovered though was the number zero. This made writing and
+ * dating extensive histories of their exploits slightly more challenging, but the
+ * system of numbers they came up with is still in use today. For example the BBC
+ * uses Roman numerals to date their programmes.
+ *
+ * The Romans wrote numbers using letters - I, V, X, L, C, D, M. (notice these
+ * letters have lots of straight lines and are hence easy to hack into stone tablets)
+ *
+ * Given a decimal number, convert to a Roman numeral representation. For example,
+ * in Roman numerals, decimal 1999 becomes "MCMXCIX". BONUS: Given a Roman numeral,
+ * convert back into a decimal number.
+ *
+ * Useful references:
+ *
+ *  - [Roman Numerals @ NovaRoma.org](http://www.novaroma.org/via_romana/numbers.html)
+ *  - [Roman Numerals @ Wikipedia.org](http://en.wikipedia.org/wiki/Roman_numerals)
+ */
+console.log('Hello code!');
---
+++ 2013-04-27-tests.js
@@ -0,0 +1,4 @@
+var assert = require("assert"),
+    problem = require("./2013-04-27-code");
+    
+console.log('Hello tests!');
