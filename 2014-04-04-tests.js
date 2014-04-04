347d0f32fd16de2045e699e399c44a5441e6fb6d
David Rogers <al.the.x@gmail.com>
2014-04-04 10:52:44 -0400

    @cguidone @jeepcrew

---
+++ 2014-04-04-tests.js
@@ -0,0 +1,18 @@
+var assert = require('assert'),
+    hallway = require('./2014-04-04-code.js');
+
+/**
+* First test to make sure all doors in Hallway are closed
+* Door #  | open(1)/closed(-1)
+* 1  | -1
+* 2  | -1
+* 3  | -1
+* 4  | -1
+* 5  | -1
+* 6  | -1
+* 7  | -1
+* 8  | -1
+* 9  | -1
+* 10 | -1
+ 
+*/ 
