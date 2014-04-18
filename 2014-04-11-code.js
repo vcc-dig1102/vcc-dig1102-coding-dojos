c7c4754c24aa84d926d70527955863ce95b121b3
David Rogers <al.the.x@gmail.com>
2014-04-18 09:46:14 -0400

    Retro from last week

--- 2014-04-11-code.js
+++ 2014-04-11-code.js
@@ -1,3 +1,23 @@
+/**
+ * What did we do well?
+ * - planning with pseudocode
+ * - examining the problem
+ * - got farther!
+ * - refactoring
+ * 
+ * What should we improve?
+ * - variable names
+ * - try the second trip
+ * - try displaying? (OMG!)
+ * - use ITERATION
+ * - how do we test a hallway with X doors?
+ * 
+ * Did we meet our goals?
+ * - learn: `this.`, `[ ] != [ ]; // WTF?`, `assert.deepEqual()`, 5 ways to do anything!
+ * - practice: refactoring, writing tests, building arrays, `for` loops
+ * - fun: YAY!
+ * - solve: BOO!
+ */
 module.exports = {
     CLOSED: -1,
     OPENED: +1,
