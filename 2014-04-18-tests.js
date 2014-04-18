26db762fa0e3383e94bb9efdf3ec8a568a49b34d
David Rogers <al.the.x@gmail.com>
2014-04-18 10:31:16 -0400

    New dojo!

---
+++ 2014-04-18-tests.js
@@ -0,0 +1,16 @@
+/**
+ * Retrospective:
+ * 
+ * What did we do well?
+ * 
+ * 
+ * What should we improve?
+ * 
+ * 
+ * Did we meet our goals?
+ * 
+ *
+ */
+var assert = require('assert'),
+    hallway = require('./2014-04-18-code.js');
+    
